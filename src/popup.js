900e3b238f3064580e32b8cf0ed7291fddd48313
Nick Trombley <trombley.nick@gmail.com>
2020-02-02 18:37:29 -0500

    Add content script

--- src/popup.js
+++ src/popup.js
@@ -1,3 +1,8 @@
-console.log("What's up?");
-
-document.getElementById('test').innerText = 'Hello there';
+/**
+ * When the popup loads, inject a content script into the active tab,
+ * and add a click handler.
+ * If we couldn't inject the script, handle the error.
+ */
+browser.tabs.executeScript({ file: '/content_scripts/index.js' });
+// .then(listenForClicks)
+// .catch(reportExecuteScriptError);
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
 	entry: {
 		popup: './src/popup.js',
+		content_scripts: './src/content.js',
 	},
 	output: {
 		path: path.resolve(__dirname, 'addon'),
